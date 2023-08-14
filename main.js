950d906f8bac5e5dacd75ed61d7aac0c9a7486b9
mogamoga <mogamoga.g1thu6@gmail.com>
2023-08-14 19:19:00 +0900

    file

--- main.js
+++ main.js
@@ -3,4 +3,15 @@ const monoCanvas = new MonochromeCanvas(document.querySelector("canvas"));
 
 monoCanvas.setImage("野獣先輩.png");
 
+const inputFile = document.querySelector("input[type='file']");
 
+inputFile.addEventListener("change", function(e) {
+    const file = e.target.files;
+    const reader = new FileReader();
+
+    reader.readAsDataURL(file[0]);
+
+    reader.onload = function() {
+        monoCanvas.setImage(reader.result);
+    }
+});
