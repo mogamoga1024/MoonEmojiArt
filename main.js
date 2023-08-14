67658e6250ba5b3cb4aef7d1d03578d63f9a4fb8
mogamoga <mogamoga.g1thu6@gmail.com>
2023-08-15 02:35:05 +0900

    いったんコミット

--- main.js
+++ main.js
@@ -7,7 +7,13 @@ const App = {
             file: null,
             fileReader: null,
             baseAverageColor: 90,
+            baseAverageColorDefault: 90,
+            baseAverageColorMin: 0,
+            baseAverageColorMax: 255,
             baseColorDistance: 50,
+            baseColorDistanceDefault: 50,
+            baseColorDistanceMin: 0,
+            baseColorDistanceMax: 200,
             needOutline: true,
         }
     },
@@ -19,6 +25,32 @@ const App = {
         onChangeInputFile(e) {
             this.file = e.target.files[0];
         },
+        onChangeBaseAverageColorNumber(e) {
+            if (e.target.value === "") {
+                this.baseAverageColor = this.baseAverageColorDefault;
+                return;
+            }
+            const value = Number(e.target.value);
+            if (value < this.baseAverageColorMin) {
+                this.baseAverageColor = this.baseAverageColorMin;
+            }
+            else if (value > this.baseAverageColorMax) {
+                this.baseAverageColor = this.baseAverageColorMax;
+            }
+        },
+        onChangeBaseColorDistanceNumber(e) {
+            if (e.target.value === "") {
+                this.baseColorDistance = this.baseColorDistanceDefault;
+                return;
+            }
+            const value = Number(e.target.value);
+            if (value < this.baseColorDistanceMin) {
+                this.baseColorDistance = this.baseColorDistanceMin;
+            }
+            else if (value > this.baseAverageColorMax) {
+                this.baseColorDistance = this.baseColorDistanceMax;
+            }
+        }
     }
 };
 
