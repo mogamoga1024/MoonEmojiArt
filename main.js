ab75adeaeca30eb9e029996aeb79c405db0788c6
mogamoga <mogamoga.g1thu6@gmail.com>
2023-08-14 21:08:54 +0900

    今日も一日おつねばだよー

--- main.js
+++ main.js
@@ -4,10 +4,24 @@ const monoCanvas = new MonochromeCanvas(document.querySelector("canvas"));
 monoCanvas.monochrome("野獣先輩.png");
 
 const inputFile = document.querySelector("input[type='file']");
+const baseAverageColorRange = document.querySelector("#base-average-color-range");
+const monochromeButton = document.querySelector("#monochrome-btn");
+
+let file = null;
+const reader = new FileReader();
 
 inputFile.addEventListener("change", function(e) {
-    const file = e.target.files[0];
-    const reader = new FileReader();
+    file = e.target.files[0];
+});
+
+baseAverageColorRange.addEventListener("change", function(e) {
+    monoCanvas.baseAverageColor = Number(e.target.value);
+});
+
+monochromeButton.addEventListener("click", function() {
+    if (file === null) {
+        return;
+    }
 
     reader.readAsDataURL(file);
 
@@ -15,3 +29,4 @@ inputFile.addEventListener("change", function(e) {
         monoCanvas.monochrome(reader.result);
     }
 });
+
