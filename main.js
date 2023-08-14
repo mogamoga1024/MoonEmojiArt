7997ca4121902ca80f8ae668eb9b776a93b86553
mogamoga <mogamoga.g1thu6@gmail.com>
2023-08-14 21:59:22 +0900

    あ

--- main.js
+++ main.js
@@ -5,11 +5,13 @@ monoCanvas.monochrome("野獣先輩.png");
 
 const inputFile = document.querySelector("input[type='file']");
 const baseAverageColorRange = document.querySelector("#base-average-color-range");
-const baseColorDistanceRange = document.querySelector("#base-color-distance");
+const baseAverageColorNumber = document.querySelector("#base-average-color-number");
+const baseColorDistanceRange = document.querySelector("#base-color-distance-range");
+const baseColorDistanceNumber = document.querySelector("#base-color-distance-number");
 const monochromeButton = document.querySelector("#monochrome-btn");
 
-baseAverageColorRange.value = monoCanvas.baseAverageColor;
-baseColorDistanceRange.value = monoCanvas.baseColorDistance;
+baseAverageColorRange.value = baseAverageColorNumber.value = monoCanvas.baseAverageColor;
+baseColorDistanceRange.value = baseColorDistanceNumber.value = monoCanvas.baseColorDistance;
 
 let file = null;
 const reader = new FileReader();
@@ -20,10 +22,44 @@ inputFile.addEventListener("change", function(e) {
 
 baseAverageColorRange.addEventListener("change", function(e) {
     monoCanvas.baseAverageColor = Number(e.target.value);
+    baseAverageColorNumber.value = monoCanvas.baseAverageColor;
+});
+
+baseAverageColorNumber.addEventListener("change", function(e) {
+    let value = Number(e.target.value);
+    const min = Number(baseAverageColorRange.min);
+    const max = Number(baseAverageColorRange.max);
+    if (value < min) {
+        value = min;
+        baseAverageColorNumber.value = value;
+    }
+    else if (value > max) {
+        value = max;
+        baseAverageColorNumber.value = value;
+    }
+    monoCanvas.baseAverageColor = value;
+    baseAverageColorRange.value = monoCanvas.baseAverageColor;
 });
 
 baseColorDistanceRange.addEventListener("change", function(e) {
     monoCanvas.baseColorDistance = Number(e.target.value);
+    baseColorDistanceNumber.value = monoCanvas.baseColorDistance;
+});
+
+baseColorDistanceNumber.addEventListener("change", function(e) {
+    let value = Number(e.target.value);
+    const min = Number(baseColorDistanceRange.min);
+    const max = Number(baseColorDistanceRange.max);
+    if (value < min) {
+        value = min;
+        baseColorDistanceNumber.value = value;
+    }
+    else if (value > max) {
+        value = max;
+        baseColorDistanceNumber.value = value;
+    }
+    monoCanvas.baseColorDistance = value;
+    baseColorDistanceRange.value = monoCanvas.baseColorDistance;
 });
 
 monochromeButton.addEventListener("click", function() {
