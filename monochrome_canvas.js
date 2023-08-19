f0cefc9e534de08bbde59d239571286c623a0786
mogamoga <mogamoga.g1thu6@gmail.com>
2023-08-20 03:13:08 +0900

    かの皇帝も言っていた。不可能なんて絶対ないって

--- monochrome_canvas.js
+++ monochrome_canvas.js
@@ -58,9 +58,85 @@ class MonochromeCanvas {
         this.#context.textAlign = "center";
         this.#context.fillText(text, this.#canvas.width / 2, 0);
 
+        const trimming = this.#trimming(this.pixels);
+
         dstCanvas.width = this.#canvas.width;
         dstCanvas.height = this.#canvas.height;
-        dstContext.putImageData(this.#context.getImageData(0, 0, this.#canvas.width, this.#canvas.height), 0, 0);
+        dstContext.putImageData(this.#context.getImageData(trimming.x, trimming.y, trimming.width, trimming.height), 0, 0);
+    }
+
+    #trimming(pixels) {
+        const data = pixels.data;
+        let targetLeftX = -1;
+        let targetRightX = -1;
+        let targetTopY = -1;
+        let targetBottomY = -1;
+
+        // left-xを求める
+        for (let col = 0; col < pixels.width; col++) {
+            for (let row = 0; row < pixels.height; row++) {
+                const i = row * pixels.width * 4 + col * 4;
+                if (data[i] === 0) {
+                    targetLeftX = col;
+                    break;
+                }
+            }
+            if (targetLeftX !== -1) {
+                break;
+            }
+        }
+
+        if (targetLeftX === -1) {
+            throw new Error("文字がない！！真っ白！！");
+        }
+
+        // right-xを求める
+        for (let col = pixels.width - 1; col >= 0; col--) {
+            for (let row = 0; row < pixels.height; row++) {
+                const i = row * pixels.width * 4 + col * 4;
+                if (data[i] === 0) {
+                    targetRightX = col;
+                    break;
+                }
+            }
+            if (targetRightX !== -1) {
+                break;
+            }
+        }
+
+        // top-yを求める
+        for (let row = 0; row < pixels.height; row++) {
+            for (let col = targetLeftX; col <= targetRightX; col++) {
+                const i = row * pixels.width * 4 + col * 4;
+                if (data[i] === 0) {
+                    targetTopY = row;
+                    break;
+                }
+            }
+            if (targetTopY !== -1) {
+                break;
+            }
+        }
+
+        // bottom-yを求める
+        for (let row = pixels.height - 1; row >= 0; row--) {
+            for (let col = targetLeftX; col <= targetRightX; col++) {
+                const i = row * pixels.width * 4 + col * 4;
+                if (data[i] === 0) {
+                    targetBottomY = row;
+                    break;
+                }
+            }
+            if (targetBottomY !== -1) {
+                break;
+            }
+        }
+
+        return {
+            x: targetLeftX, y: targetTopY,
+            width: targetRightX - targetLeftX + 1,
+            height: targetBottomY - targetTopY + 1
+        };
     }
 
     #tateText(text, font, negativeMargin) {
