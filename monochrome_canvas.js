8a0c8d1f2323972f622762e743b1ef65338f20cf
mogamoga <mogamoga.g1thu6@gmail.com>
2023-08-21 20:09:02 +0900

    あづい～～

--- monochrome_canvas.js
+++ monochrome_canvas.js
@@ -41,8 +41,8 @@ class MonochromeCanvas {
     #tateText(text, font) {
         const margin = 4;
 
-        // const tmpCanvas = document.createElement("canvas");
-        const tmpCanvas = document.querySelector("#canvas");
+        const tmpCanvas = document.createElement("canvas");
+        // const tmpCanvas = document.querySelector("#canvas");
         const tmpContext = tmpCanvas.getContext("2d", { willReadFrequently: true });
         
         let minCanvasHeight = 0;
@@ -59,12 +59,6 @@ class MonochromeCanvas {
             minCanvasHeight = Math.ceil(Math.abs(measure.actualBoundingBoxAscent) + measure.actualBoundingBoxDescent);
             tmpCanvas.height = minCanvasHeight;
 
-            console.log("font:", font);
-            console.log("あ measure w:", measure.width);
-            console.log("あ measure box asc:", measure.actualBoundingBoxAscent);
-            console.log("あ measure box dsc:", measure.actualBoundingBoxDescent);
-            console.log("tmpCanvas w h:", tmpCanvas.width, tmpCanvas.height);
-
             tmpContext.font = font;
             tmpContext.fillStyle = "#fff";
             tmpContext.fillRect(0, 0, tmpCanvas.width, tmpCanvas.height);
@@ -75,25 +69,6 @@ class MonochromeCanvas {
             const tmpPixels = tmpContext.getImageData(0, 0, tmpCanvas.width, tmpCanvas.height)
             return this.#trimming(tmpPixels);
         })();
-
-        console.log("standardChar y w h:", standardCharY, standardCharWidth, standardCharHeight);
-
-        // debug start
-
-        // this.#canvas.width = 500;
-        // this.#canvas.height = 500;
-
-        // this.#context.font = font;
-        // this.#context.fillStyle = "#fff";
-        // this.#context.fillRect(0, 0, this.#canvas.width, this.#canvas.height);
-        // this.#context.fillStyle = "#000";
-        // this.#context.textBaseline = "top";
-        // this.#context.textAlign = "center";
-        // this.#context.fillText("あ", this.#canvas.width / 2, 0);
-
-        // return;
-
-        // debug end
 
         // 各文字の幅、高さの抽出とか
         let tmpCanvasWidth = 0;
@@ -106,11 +81,6 @@ class MonochromeCanvas {
             const measure = tmpContext.measureText(char);
             const width = measure.width;
             const height = Math.abs(measure.actualBoundingBoxAscent) + measure.actualBoundingBoxDescent;
-
-            console.log("「 measure w:", measure.width);
-            console.log("「 measure box asc:", measure.actualBoundingBoxAscent);
-            console.log("「 measure box dsc:", measure.actualBoundingBoxDescent);
-
             charList.push({
                 value: char,
                 width: width,
@@ -134,10 +104,7 @@ class MonochromeCanvas {
             const isSmallChar = "、。っゃゅょぁぃぅぇぉッャュョァィゥェォ".includes(char.value);
 
             this.#canvas.width = Math.ceil(char.width);
-            // this.#canvas.height = Math.max(Math.ceil(char.height), standardCharY + standardCharHeight);
             this.#canvas.height = Math.max(Math.ceil(char.height), minCanvasHeight);
-
-            console.log("canvas w h:", this.#canvas.width, this.#canvas.height);
 
             // テキスト反映
             this.#context.font = font;
@@ -149,15 +116,6 @@ class MonochromeCanvas {
             this.#context.fillText(char.value, this.#canvas.width / 2, 0);
             // トリミング
             const trimmed = this.#trimming(this.pixels);
-
-            // 漢数字の「一」みたいな文字は必要な余白すら切り取られてしまうので対策
-            // if (!isSmallChar) {
-            //     if (trimmed.height < standardCharHeight) {
-            //         // trimmed.y = standardCharY;
-            //         trimmed.y -= (standardCharHeight - trimmed.height) / 2;
-            //         trimmed.height = standardCharHeight;
-            //     }
-            // }
 
             // 転写
             let dstX = (tmpCanvas.width - trimmed.width) / 2;
@@ -187,8 +145,6 @@ class MonochromeCanvas {
             if (maxWidth < trimmed.width) {
                 maxWidth = trimmed.width;
             }
-
-            console.log("trimmed:", trimmed);
         }
 
         let yokoMargin = margin;
