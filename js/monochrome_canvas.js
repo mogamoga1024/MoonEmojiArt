7639c435719783bb2db97d14578e19857e66d132
mogamoga <mogamoga.g1thu6@gmail.com>
2023-08-26 20:56:45 +0900

    trimmingをUtil化

--- js/monochrome_canvas.js
+++ js/monochrome_canvas.js
@@ -79,7 +79,7 @@ class MonochromeCanvas {
             tmpContext.textAlign = "center";
             tmpContext.fillText("あ", tmpCanvas.width / 2, 0);
             const tmpPixels = tmpContext.getImageData(0, 0, tmpCanvas.width, tmpCanvas.height)
-            return this.#trimming(tmpPixels);
+            return CanvasUtils.trimming(tmpPixels);
         })();
 
         // 各文字の幅、高さの抽出とか
@@ -156,7 +156,7 @@ class MonochromeCanvas {
 
             this.#context.fillText(char.value, this.#canvas.width / 2, this.#canvas.height / 2);
             // トリミング
-            const trimmed = this.#trimming(this.pixels);
+            const trimmed = CanvasUtils.trimming(this.pixels);
 
             // 転写
             let dstX = (tmpCanvas.width - trimmed.width) / 2;
@@ -210,80 +210,6 @@ class MonochromeCanvas {
         this.#context.drawImage(tmpCanvas2, 0, 0, this.#canvas.width, this.#canvas.height);
     }
 
-    #trimming(pixels) {
-        const data = pixels.data;
-        let targetLeftX = -1;
-        let targetRightX = -1;
-        let targetTopY = -1;
-        let targetBottomY = -1;
-
-        // left-xを求める
-        for (let col = 0; col < pixels.width; col++) {
-            for (let row = 0; row < pixels.height; row++) {
-                const i = row * pixels.width * 4 + col * 4;
-                if (data[i] !== 255) {
-                    targetLeftX = col;
-                    break;
-                }
-            }
-            if (targetLeftX !== -1) {
-                break;
-            }
-        }
-
-        if (targetLeftX === -1) {
-            throw new Error("文字がない！！真っ白！！");
-        }
-
-        // right-xを求める
-        for (let col = pixels.width - 1; col >= 0; col--) {
-            for (let row = 0; row < pixels.height; row++) {
-                const i = row * pixels.width * 4 + col * 4;
-                if (data[i] !== 255) {
-                    targetRightX = col;
-                    break;
-                }
-            }
-            if (targetRightX !== -1) {
-                break;
-            }
-        }
-
-        // top-yを求める
-        for (let row = 0; row < pixels.height; row++) {
-            for (let col = targetLeftX; col <= targetRightX; col++) {
-                const i = row * pixels.width * 4 + col * 4;
-                if (data[i] !== 255) {
-                    targetTopY = row;
-                    break;
-                }
-            }
-            if (targetTopY !== -1) {
-                break;
-            }
-        }
-
-        // bottom-yを求める
-        for (let row = pixels.height - 1; row >= 0; row--) {
-            for (let col = targetLeftX; col <= targetRightX; col++) {
-                const i = row * pixels.width * 4 + col * 4;
-                if (data[i] !== 255) {
-                    targetBottomY = row;
-                    break;
-                }
-            }
-            if (targetBottomY !== -1) {
-                break;
-            }
-        }
-
-        return {
-            x: targetLeftX, y: targetTopY,
-            width: targetRightX - targetLeftX + 1,
-            height: targetBottomY - targetTopY + 1
-        };
-    }
-
     #yokoText(text, font, tatePixelCount) {
         this.#context.font = font;
         this.#context.textBaseline = "top";
@@ -300,7 +226,7 @@ class MonochromeCanvas {
         this.#context.textAlign = "center";
         this.#context.fillText(text, this.#canvas.width / 2, this.#canvas.height / 2);
 
-        const trimmed = this.#trimming(this.pixels);
+        const trimmed = CanvasUtils.trimming(this.pixels);
         const pixels = this.#context.getImageData(0, trimmed.y, this.#canvas.width, trimmed.height);
 
         const tateMargin = 4;
