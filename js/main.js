df42f732a5332ab92218ba137bcda0960688e6ba
mogamoga <mogamoga.g1thu6@gmail.com>
2023-09-06 14:49:05 +0900

    何でも知りたいの 研究対象

--- js/main.js
+++ js/main.js
@@ -8,7 +8,8 @@ let monoCanvas = null;
 
 const MSG_NO_INPUT_DATA = 
 `・変換したい文か画像を決めて生成ボタンを押してね！
-・サイズが小さいとクオリティが低くなるよ！`;
+・サイズが小さいとクオリティが低くなるよ！
+・ちなみにYouTubeのコメントに使うとスパム判定で表示されないよ。悲しいね。`;
 const MSG_ERROR = "生成に失敗したよ！ごめんね！";
 const MSG_FAILURE_TEXT_MONO = "文字数が多すぎて一次加工で失敗したよ。減らしてね。";
 const MSG_FAILURE_IMAGE_MONO = "画像サイズが大きすぎて一次加工で失敗したよ。減らしてね。";
