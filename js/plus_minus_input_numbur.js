92c980282a0a66cfcd8eabd8824ae8b54835ddfd
mogamoga <mogamoga.g1thu6@gmail.com>
2024-03-16 02:57:02 +0900

    壊したバランスの三角形

--- js/plus_minus_input_numbur.js
+++ js/plus_minus_input_numbur.js
@@ -2,18 +2,18 @@
 const PlusMinusInputNumbur = {
     props: {
         modelValue: Number,
-        modelMin: Number,
-        modelMax: Number,
+        min: Number,
+        max: Number,
     },
-    emits: ['update:modelValue'],
+    emits: ["update:modelValue"],
     template: `
         <div class="plus-minus-input-numbur">
           <div class="minus-btn"
                @click="onClickMinusButton">-</div>
           <input type="number"
-                 :min="modelMin"
-                 :max="modelMax"
-                 :value="modelValue"
+                 :min="min"
+                 :max="max"
+                 :value="mutModelValue"
                  @blur="onBlurInputNumber">
           <div class="plus-btn"
                @click="onClickPlusButton">+</div>
@@ -21,21 +21,51 @@ const PlusMinusInputNumbur = {
     `,
     data() {
         return {
-            // todo
+            mutModelValue: 0,
+            modelPrev: 0
         }
     },
     created() {
-        // todo
+        this.mutModelValue = this.modelValue;
+        this.modelPrev = this.modelValue;
+    },
+    watch: {
+        mutModelValue(newVal) {
+            if (newVal === "") return;
+            this.modelPrev = Number(newVal);
+        },
     },
     methods: {
         onClickMinusButton() {
-            // todo
+            if (this.mutModelValue > this.min) {
+                this.mutModelValue -= 1;
+                this.$emit("update:modelValue", this.mutModelValue);
+            }
         },
         onClickPlusButton() {
-            // todo
+            if (this.mutModelValue < this.max) {
+                this.mutModelValue += 1;
+                this.$emit("update:modelValue", this.mutModelValue);
+            }
         },
-        onBlurInputNumber() {
-            // todo
+        onBlurInputNumber(e) {
+            const newVal = Number(e.target.value);
+
+            if (e.target.value === "") {
+                this.mutModelValue = this.modelPrev;
+                this.$forceUpdate();
+            }
+            else if (newVal < this.min) {
+                this.mutModelValue = this.min;
+            }
+            else if (newVal > this.max) {
+                this.mutModelValue = this.max;
+            }
+            else {
+                this.mutModelValue = newVal;
+            }
+            
+            this.$emit("update:modelValue", this.mutModelValue);
         }
     }
 };
