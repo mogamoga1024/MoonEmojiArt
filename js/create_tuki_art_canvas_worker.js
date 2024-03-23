e24d4e173313c6494b451e027f7dc6a23e4fedfb
mogamoga <mogamoga.g1thu6@gmail.com>
2024-03-24 05:52:14 +0900

    高カロリー栄養剤

--- js/create_tuki_art_canvas_worker.js
+++ js/create_tuki_art_canvas_worker.js
@@ -1,15 +1,11 @@
 importScripts("constants.js");
-// importScripts("canvas_utils.js");
 importScripts("tuki_art_generator.js");
 
 onmessage = async e => {
     const textList = e.data.textList;
     const canvasParams = e.data.canvasParams;
     const context = e.data.canvas.getContext("2d", { willReadFrequently: true });
-    // debugger
     TukiArtGenerator.createTukiArtCanvas(textList, canvasParams, context);
-    // const tukiArtBlob = e.data.canvas.convertToBlob();
-    // postMessage(tukiArtBlob, [tukiArtBlob]);
     const fileReader = new FileReader();
     fileReader.onload = () => {
         postMessage(fileReader.result);
