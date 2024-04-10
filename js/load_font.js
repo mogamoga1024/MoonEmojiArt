dd1e11dabf6716300d595117d5ec367ec16c66fc
mogamoga <mogamoga.g1thu6@gmail.com>
2024-04-10 16:11:13 +0900

    CSSも読み込んでおく 一応

--- js/load_font.js
+++ js/load_font.js
@@ -6,11 +6,8 @@ https://style01.net/3037/
 */
 
 /*
-以前はHTMLのヘッダー部で
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&family=Noto+Serif+JP:wght@400;700&display=swap" rel="stylesheet">
-と読み込んでいたが、Canvasで使うときに反映されなかった。
+以前はHTMLのヘッダー部でGoogleFontのCDNを書いても
+Canvasで使うときに反映されなかった。
 ロードされていないためだと思われる。
 */
 
